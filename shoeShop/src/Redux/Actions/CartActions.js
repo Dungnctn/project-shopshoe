4646c8fe81d888851bc60cb5283cb92486471661
Chi Dung <Dungnc0402@gmail.com>
2022-04-09 00:43:34 +0700

    feat: cart

--- shoeShop/src/Redux/Actions/CartActions.js
+++ shoeShop/src/Redux/Actions/CartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CART_ADD_ITEM } from '../Constants/CartConstans';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../Constants/CartConstans';
 
 export const addToCart = (id, qty) => async(dispatch, getState) => {
     const {data} = await axios.get(`/api/products/${id}`);
@@ -14,6 +14,15 @@ export const addToCart = (id, qty) => async(dispatch, getState) => {
             product: data._id,
             qty
         }
+    })
+    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
+}
+
+// REMOVE PRODUCT FROM CART
+export const removefromcart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id
     })
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
 }
--- shoeShop/src/screens/CartScreen.js
+++ shoeShop/src/screens/CartScreen.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import Header from "./../components/Header";
 import { Link } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux'
-import { addToCart } from "../Redux/Actions/CartActions";
+import { addToCart, removefromcart } from "../Redux/Actions/CartActions";
 
-const CartScreen = ({match, location}) => {
+const CartScreen = ({match, location, history}) => {
   window.scrollTo(0, 0);
   const productId = match.params.id
   const dispatch = useDispatch()
@@ -13,11 +13,22 @@ const CartScreen = ({match, location}) => {
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart
   
+  const total = cartItems.reduce((a,i) => a + i.qty * i.price, 0).toFixed(2)
+
   useEffect(() => {
     if(productId){
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
+
+  const checkOutHandle = () => {
+    history.push("/login?redirect=shipping")
+  }
+
+  const removeFromCartHandle = (id) => {
+    dispatch(removefromcart(id))
+  }
+
   return (
     <>
       <Header />
@@ -48,7 +59,9 @@ const CartScreen = ({match, location}) => {
                 {
                   cartItems.map((item) => (
                 <div className="cart-iterm row">
-                  <div className="remove-button d-flex justify-content-center align-items-center">
+                  <div 
+                  onClick={() => removeFromCartHandle(item.product)}
+                  className="remove-button d-flex justify-content-center align-items-center">
                     <i className="fas fa-times"></i>
                   </div>
                   <div className="cart-image col-md-3">
@@ -61,36 +74,42 @@ const CartScreen = ({match, location}) => {
                   </div>
                   <div className="cart-qty col-md-2 col-sm-5 mt-md-5 mt-3 mt-md-0 d-flex flex-column justify-content-center">
                     <h6>QUANTITY</h6>
-                    <select>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
+                    <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value))) } >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
                     </select>
                   </div>
                   <div className="cart-price mt-3 mt-md-0 col-md-2 align-items-sm-end align-items-start  d-flex flex-column justify-content-center col-sm-7">
-                    <h6>SUBTOTAL</h6>
-                    <h4>$456</h4>
+                    <h6>PRICE</h6>
+                    <h4>$ {item.price}</h4>
                   </div>
+                  
                 </div>
+                
                 ))
               }
-
                 <div className="total">
                   <span className="sub">total:</span>
-                  <span className="total-price">$567</span>
+                  <span className="total-price">$ {total}</span>
                 </div>
                 <hr />
                 <div className="cart-buttons d-flex align-items-center row">
                   <Link to="/" className="col-md-6 ">
                     <button>Continue To Shopping</button>
                   </Link>
-                  <div className="col-md-6 d-flex justify-content-md-end mt-3 mt-md-0">
-                    <button>
-                      <Link to="/shipping" className="text-white">
-                        Checkout
-                      </Link>
-                    </button>
-                  </div>
+                  {
+                    total > 0 && (
+                      <div className="col-md-6 d-flex justify-content-md-end mt-3 mt-md-0">
+                        <button onClick={() => checkOutHandle}>
+                            Checkout
+                        </button>
+                      </div>
+                    ) 
+                  }
+                  
                 </div>
                 
               </>
