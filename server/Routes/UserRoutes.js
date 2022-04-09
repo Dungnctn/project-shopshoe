024b81bc0927bada25ea2f7209eff37a4925853b
Chi Dung <Dungnc0402@gmail.com>
2022-04-10 00:27:51 +0700

    feat: login token

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -1,29 +1,50 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
+import protect from '../Middleware/AuthMiddleware.js';
 import User from '../models/UserModel.js'
+import generateToken from '../utils/generateToken..js';
 
-const productRoute = express.Router();
+const userRoute = express.Router();
 
 // LOGIN
-productRoute.post("/login", asyncHandler(
-    async (req, res) => {
+userRoute.post("/login", asyncHandler( async (req, res) => {
         const {email, password} = req.body;
-        const user = await User.findOne({email}).exec();
+        const user = await User.findOne({ email });
         if(user && (await user.matchPassword(password)) ){
             res.json({
                 _id: user._id,
                 name: user.name,
                 email: user.email,
                 isAdmin: user.isAdmin,
-                token: null,
-                createAt: user.createAt,
+                token: generateToken(user._id),
+                createdAt: user.createdAt,
             })
-        } else {
-
+        } else { 
+            res.status(401)
+            throw new Error("Sai Email hoac Password");
         }
     }
 ))
 
+// PROFILE
+userRoute.get("/profile", 
+    protect,
+    asyncHandler( async (req, res) => {
+        const user = await User.findById(req.user._id)
+        if(user) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+                createdAt: user.createdAt,
+            })
+        } else {
+            res.status(404);
+            throw new Error("User not found")
+        }
 
+} ))
 
-export default productRoute
+export default userRoute
--- server/server.js
+++ server/server.js
@@ -3,15 +3,20 @@ import dotenv from "dotenv"
 import connectDatabase from "./config/MongoDB.js";
 import ImportData from "./Datatlmport.js.js";
 import productRoute from "./Routes/ProductRoutes.js";
+import userRouter from "./Routes/UserRoutes.js";
 import { errorHandle, notFound } from "./Middleware/Errors.js";
 
 dotenv.config();
 connectDatabase();
 const app = express();
+app.use(express.json())
 
 // API
 app.use("/api/import", ImportData);
 app.use("/api/products", productRoute);
+app.use("/api/users", userRouter);
+
+// ERROR HANDLE
 app.use(notFound);
 app.use(errorHandle);
 
